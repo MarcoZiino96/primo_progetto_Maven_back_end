49d703f88e57907cff848163f53245269f2f5c6f
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-01-18 22:42:10 +0100

    consegna esercizio g4 s2

--- src/main/java/week2/day_4/Main.java
+++ src/main/java/week2/day_4/Main.java
@@ -1,14 +1,16 @@
 package week2.day_4;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.OptionalDouble;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Product p1 = new Product(0, "Le avventure di jo","Books", 105);
         Product p2 = new Product(1, "Sono Adam","Books", 115);
         Product p3 = new Product(2, "Libro","Books", 55);
@@ -46,24 +48,38 @@ public class Main {
 
         List<Product> productList = List.of(p1,p2,p3,p4,p5,p6,p7,p8);
         List<Order> orderList = List.of(o1,o2,o3,o4,o5,o6,o7,o8);
+        saveProductDisc(productList);
+        leggiProdottiNelDisco();
+
 
         //Esercizio1##
-
         Map<Costumer,List<Order>> orderForClient = orderList.stream().collect(Collectors.groupingBy(Order::getCostumer));
 
         //Esercizio2##
         Map<Costumer, Double> costOrderClient =  orderList.stream().collect(Collectors.
                 groupingBy(Order::getCostumer, Collectors.summingDouble(o->o.getProducts().stream()
                         .mapToDouble(Product::getPrice).sum())));
+        //Esercizio3##
+
+        Product pMaxPrice = productList.stream().collect(Collectors.maxBy(Comparator.comparingDouble(Product::getPrice))).get();
+
         //Esercizio4##
-        Double costoMedioOrderClient =  orderList.stream().collect(Collectors.
-                groupingBy(Order::getProducts));
+        Double costoMedioOrderClient = orderList.stream().collect(Collectors.averagingDouble(o->o.getProducts().stream().mapToDouble(Product::getPrice).sum()));
+
+        //Esercizio5##
+
+        Map<String, Double> costoForCategories = productList.stream().
+                collect(Collectors.groupingBy(Product::getCategory, Collectors.summingDouble(Product::getPrice)));
 
 
 
+        //System.out.println(orderForClient);
+        //System.out.println(costOrderClient);
+        //System.out.println(orderList);
+        System.out.println(pMaxPrice);
+        System.out.println(costoForCategories);
 
-        System.out.println(orderForClient);
-        System.out.println(costOrderClient);
+        System.out.println("La media dei prezzi della lista degli ordini è: "+costoMedioOrderClient);
     }
 
     public static void printListP(List<Product> list){
@@ -75,5 +91,39 @@ public class Main {
         for (Order i:list){
             System.out.println(i);
         }
+    }
+
+    public static void saveProductDisc(List<Product>list) {
+
+       try {
+           File file = new File("dirProdotti/prodotti.txt");
+           String products = list.stream().map(p->p.getName()+"@"+p.getCategory()+"@"+p.getPrice()).collect(Collectors.joining("#"));
+           FileUtils.writeStringToFile(file, products, Charset.defaultCharset(), true);
+       }catch (IOException e){
+           System.out.println(e.getMessage());
+       }
+    }
+
+    public static ArrayList<Product> leggiProdottiNelDisco(){
+
+      try {
+          File file = new File("dirProdotti/prdotti.txt");
+          String strgProducts = FileUtils.readFileToString(file, Charset.defaultCharset());
+
+          String[] strgProduct = strgProducts.split("#");
+
+          ArrayList<Product> prodotti = Arrays.stream(strgProduct).map(s -> {
+              String[] strgSingleProd = s.split("@");
+              Product p = new Product(new Random().nextLong(),
+                      strgSingleProd[0],
+                      strgSingleProd[1],
+                      Double.parseDouble(strgSingleProd[2]));
+              return p;
+          }).collect(Collectors.toCollection(ArrayList::new));
+
+          return prodotti;
+      }catch (IOException e){
+          System.out.println(e.getMessage());
+      }
     }
 }
